a608014941f81c4d21987f4dc6ee238f287a7a1e
Keijo Korhonen <kkorhonenk.k@gmail.com>
2019-06-05 12:21:10 +0300

    heroku setup

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(bodyParser.json())
 
 morgan.token('content', function (req, res) {
@@ -87,7 +89,7 @@ app.delete('/api/persons/:id', (req, res) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
