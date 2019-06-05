29f042252edeba4ced4ce3451a10042acf8bc4cb
Keijo Korhonen <kkorhonenk.k@gmail.com>
2019-06-05 12:35:39 +0300

    uibuild

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # fullstack2019osa3
+Linkki sovellukseen: https://protected-cliffs-90052.herokuapp.com/api/persons
---
+++ build/precache-manifest.eed3a33c8e19db133b8e182805e8ea28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248137ef1710d48fbf2bf754d9e8dff3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e78982a697d031e256d",
+    "url": "/static/css/main.233b8827.chunk.css"
+  },
+  {
+    "revision": "717095a3ea21a6bbdee1",
+    "url": "/static/js/2.3d2452bb.chunk.js"
+  },
+  {
+    "revision": "6e78982a697d031e256d",
+    "url": "/static/js/main.24dbf035.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
